d6d721fac0042a2b7763d185c2a3e5e340ccc38b
HumbertoCreate <avalosgalvezjorge@gmail.com>
2024-02-07 07:57:34 -0600

    Primer commit

---
+++ Sistemas de inventarios 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistemas_de_inventarios_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
